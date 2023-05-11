7b441ef53376f8e1d8a142ed20639c8920718654
Firas <firasdabbabi@gmail.com>
2023-05-11 21:34:38 +0100

    version 1.4: mkdir utils, touch constants.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import { Stack } from '@mui/material'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
     return (
-        <div>Navbar</div>
+        <p>nav</p>
     )
 }
