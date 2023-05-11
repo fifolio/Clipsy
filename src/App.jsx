fa66738b742c97b3f8b7321f4187981108cd92cb
Firas <firasdabbabi@gmail.com>
2023-05-11 21:25:10 +0100

    version 1.3: mkdir & touch components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components/index'
+
 export default function App() {
   return (
-    <h1>Clipsy</h1>
+    <BrowserRouter>
+      <Box>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" exact element={<VideoDetail />} />
+          <Route path="/channel/:id" exact element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   )
 }
