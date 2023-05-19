829d03b9cc4f32d5b80c404e9ead26693b66513e
Firas <firasdabbabi@gmail.com>
2023-05-19 20:29:56 +0100

    Version 2.7: npm i react-player

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import ReactPlayer from 'react-player'
+import { Typography, Box, Stack } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
+
+import { Video } from './'
+import { fetchFromApi } from '../utils/FetchFromApi'
+
 
 export default function VideoDetail() {
     return (
-        <div>VideoDetail</div>
+        <Box minHeight="95vh">
+            <Stack direction={{ xs: 'column', md: 'row' }}>
+                <Box flex={1}>
+                    <ReactPlayer />
+                </Box>
+            </Stack>
+        </Box>
     )
 }
